2ca60e7bfc2faa7c5e43f0558272385c4c400e9f
Daniela Fernández <liquiddookie.17@gmail.com>
2024-08-22 19:38:04 -0300

    Updated.

---
+++ TP01/tp_01_ejercicio_02_JDBC_MySQL/src/main/java/Select.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
+
+public class Select {
+    public static void main(String[] args) {
+        String driver = "com.mysql.jdbc.Driver";
+
+        try {
+            Class.forName(driver).getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+                 | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        String uri = "jdbc:mysql://localhost:3306/example_db";
+
+        try {
+            Connection conn = DriverManager.getConnection(uri, "root", "password");
+
+            String select = "SELECT * FROM Persona";
+            PreparedStatement ps = conn.prepareStatement(select);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + ", " + rs.getString(2) + ", " + rs.getInt(3));
+            }
+
+            conn.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
